8bbf6df75cdea94cad4e74425bc4c9a7922d2544
shriyagp <162720658+shriyagp@users.noreply.github.com>
2025-03-28 13:32:56 +0530

    Create Demo2.java

---
+++ Demo2.java
@@ -0,0 +1,10 @@
+class Demo2
+{
+    public static void main(String[] args)
+    {
+        System.out.println(“Hello, World”);
+        System.out.println(“are we out of the woods?”);
+        System.out.println(“who run the world?GIRLS”);
+        System.out.println(“YOU GO GIRLLL”);
+    }
+}
