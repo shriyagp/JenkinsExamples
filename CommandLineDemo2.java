8582d2a682a32488b07fd4e90b32979ed243537d
shriyagp <162720658+shriyagp@users.noreply.github.com>
2025-03-29 12:30:34 +0530

    Create CommandLineDemo2.java

---
+++ CommandLineDemo2.java
@@ -0,0 +1,15 @@
+class CommandLineDemo2
+{
+public static void main(String args[])
+{
+String name=(args[0]);
+String rollno=(args[1]);
+String dept=(args[2]);
+
+System.out.println("Name:"+name);
+System.out.println("Roll Number:"+rollno);
+System.out.println("Department:"+dept);
+}
+}
+
+
